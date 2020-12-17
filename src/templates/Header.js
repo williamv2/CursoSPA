73845c09094ba5f0d90e196f0b0a9d9b1e560fe4
wv2 <whvelandia94@gmail.com>
2020-12-17 16:12:37 -0500

    Error404 y Routes

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,22 +1,21 @@
 const Header = () => {
-    const view = `
-        <div class="Header-main">
-            <div class="Header-logo">
-                <h1>
-                    <a href="/">
-                        100tifi.co
-                    </a>
-                </h1>
-            </div>
-            <div
-            class="Header-nav">
-            <a href="#/about/">
-                About
+const view = `
+<div class="Header-main">
+    <div class="Header-logo">
+        <h1>
+            <a href="/">
+                100tifi.co
             </a>
-        </div>
-        </div>
-    `;
-    return view;
+        </h1>
+    </div>
+    <div class="Header-nav">
+        <a href="#/about/">
+            About
+        </a>
+    </div>
+</div>
+`;
+return view;
 };
 
 export default Header;
